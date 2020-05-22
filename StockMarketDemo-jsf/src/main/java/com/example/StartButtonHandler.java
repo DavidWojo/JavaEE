52469e78ea8a9e2278ad9397ce3a1dbe517da14f
Wojciechowski, David <davidwojo15@gmail.com>
2020-05-21 22:20:45 -0700

    update

---
+++ StockMarketDemo-jsf/src/main/java/com/example/StartButtonHandler.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@RequestScoped
+public class StartButtonHandler implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	public void printIt()
+	{
+		System.out.println("Event Handler");
+	}
+}
---
+++ StockMarketDemo-jsf/src/main/java/com/example/StockBean.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class StockBean
+{
+	private String name;
+	private String price;
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getPrice()
+	{
+		return price;
+	}
+	
+	public void setPrice(String price)
+	{
+		this.price = price;
+	}
+}
